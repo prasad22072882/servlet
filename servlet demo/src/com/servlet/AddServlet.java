d3b09ebaad93889d86605dc3eabe086b3176d156
prasad <prasad22072882@gmail.com>
2019-12-12 14:58:03 +0530

    call a servlet from servlet, get method, sendRedirect(), HttpSession

--- servlet demo/src/com/servlet/AddServlet.java
+++ servlet demo/src/com/servlet/AddServlet.java
@@ -8,12 +8,15 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class AddServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
 		int a = Integer.parseInt(req.getParameter("num1"));
 		int b = Integer.parseInt(req.getParameter("num2"));
 		int sum = a + b;
-		res.sendRedirect("square?sum=" + sum); // URL rewrite
+		HttpSession session = req.getSession();
+		session.setAttribute("sum", sum);
+		res.sendRedirect("square");
 	}
 }
--- servlet demo/src/com/servlet/SqServlet.java
+++ servlet demo/src/com/servlet/SqServlet.java
@@ -6,12 +6,17 @@ import java.io.PrintWriter;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class SqServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
-		int num = Integer.parseInt(req.getParameter("sum"));
+		HttpSession session = req.getSession();
+		int num = (int) session.getAttribute("sum");
 		int result = num * num;
 		PrintWriter out = res.getWriter();
 		out.println("Result is: " + result);
+		
+		session.removeAttribute("sum"); // We can remove attribute and after that we can't fetch the value
+		out.println((int) session.getAttribute("sum")); //will through NullPointerException
 	}
 }
