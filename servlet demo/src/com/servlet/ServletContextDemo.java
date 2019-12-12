45cd7f0023c25cbcf89649c3be1a6492fc66cef6
prasad <prasad22072882@gmail.com>
2019-12-12 16:38:30 +0530

    ServletConfig and ServletContext, web.xml

--- servlet demo/src/com/servlet/ServletContextDemo.java
+++ servlet demo/src/com/servlet/ServletContextDemo.java
@@ -3,6 +3,7 @@ package com.servlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -10,10 +11,15 @@ import javax.servlet.http.HttpServletResponse;
 
 public class ServletContextDemo extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
-		ServletContext scontext = getServletContext();
-		String name = scontext.getInitParameter("name"); // Get values from web.xml file
-		String phone = scontext.getInitParameter("phone");
+		ServletConfig sconfig = getServletConfig();
+		String name = sconfig.getInitParameter("name"); // Get values from web.xml file
+		String phone = sconfig.getInitParameter("phone");
 		PrintWriter out = res.getWriter();
-		out.print("Hi.. my name is " + name + " and i am using " + phone + " phone.");
+		out.println("Hi.. my name is " + name + " and i am using " + phone + " phone.");
+
+		ServletContext scontext = getServletContext();
+		String globalName = scontext.getInitParameter("name");
+		String globalPhone = scontext.getInitParameter("phone");
+		out.println("Hi.. my name is " + globalName + " and i am using " + globalPhone + " phone.");
 	}
 }
